8c252c770705d3da728378ebeb78df6eb7dbf785
aman <aman5537jains@gmail.com>
2024-06-24 14:30:50 +0530

    chanes

--- src/Views/menu-html.blade.php
+++ src/Views/menu-html.blade.php
@@ -4,7 +4,7 @@ $currentUrl = url()->current();
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
 
-<link href="{{asset('public/vendor/harimayco-menu/style.css')}}" rel="stylesheet">
+<link href="{{asset('vendor/harimayco-menu/style.css')}}" rel="stylesheet">
 <div id="hwpwrap">
 	<div class="custom-wp-admin wp-admin wp-core-ui js   menu-max-depth-0 nav-menus-php auto-fold admin-bar">
 		<div id="wpwrap">
--- src/Views/scripts.blade.php
+++ src/Views/scripts.blade.php
@@ -30,6 +30,6 @@
 </script>
 
 {{-- <script type="text/javascript" src="{{asset('public/vendor/harimayco-menu/scripts2.js')}}"></script> --}}
-<script type="text/javascript" src="{{asset('public/vendor/harimayco-menu/scripts.js')}}"></script>
+<script type="text/javascript" src="{{asset('vendor/harimayco-menu/scripts.js')}}"></script>
 
-<script type="text/javascript" src="{{asset('public/vendor/harimayco-menu/menu.js')}}"></script>
+<script type="text/javascript" src="{{asset('vendor/harimayco-menu/menu.js')}}"></script>
